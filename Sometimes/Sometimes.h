ee748c94f87b4c76a791901b550d13d660b3c158
Alejandro Martinez <alexito4@gmail.com>
2015-11-24 20:44:31 +0000

    Initial Commit

---
+++ Sometimes/Sometimes.h
@@ -0,0 +1,19 @@
+//
+//  Sometimes.h
+//  Sometimes
+//
+//  Created by Alejandro Martinez on 24/11/15.
+//  Copyright © 2015 Alejandro Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sometimes.
+FOUNDATION_EXPORT double SometimesVersionNumber;
+
+//! Project version string for Sometimes.
+FOUNDATION_EXPORT const unsigned char SometimesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sometimes/PublicHeader.h>
+
+

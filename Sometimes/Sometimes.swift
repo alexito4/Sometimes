b73521116f2ffae736275e76b36ea6a1e7b12c2f
Alejandro Martinez <alexito4@gmail.com>
2015-11-26 20:41:17 +0000

    Commit tests

--- Sometimes/Sometimes.swift
+++ Sometimes/Sometimes.swift
@@ -14,23 +14,47 @@ TODO: Use a custom user defaults domain or prefix the keys.
     In oder to provide a `clean` method.
 */
 public struct SometimesKey {
-    private let valueKey: String
+    let valueKey: String
     
     public init(_ key: String) {
-        self.valueKey = key
+        self.valueKey = Sometimes.keyPrefix + key
+    }
+}
+
+/// Defines the protocol need for the object to store the keys.
+protocol SometimesStore {
+    func allKeys() -> Set<String>
+    func boolForKey(defaultName: String) -> Bool
+    mutating func setBool(value: Bool, forKey defaultName: String)
+    mutating func removeBoolForKey(defaultName: String)
+}
+
+/// Extend `NSUserDefaults` to conform to `SometimesStore`
+extension NSUserDefaults: SometimesStore {
+    func removeBoolForKey(defaultName: String) {
+        self.removeObjectForKey(defaultName)
+    }
+    func allKeys() -> Set<String> {
+        return Set(self.dictionaryRepresentation().keys)
     }
 }
 
 public struct Sometimes {
     
+    private static let keyPrefix = "sometimes_"
+    
     public typealias Block = Void -> ()
+    
+    /// Object used to store the keys.
+    /// `NSUserDefaults.standardUserDefaults()` by default.
+    static var defaults: SometimesStore = NSUserDefaults.standardUserDefaults()
     
     /**
      Executes the clousure... sometimes.
      Well, the first time.
      
-     - parameter key:  <#key description#>
-     - parameter code: <#code description#>
+     - parameter key:  uniquely identifies a block of execution
+     - parameter code: code to be run (or not)
      
      - returns: true if the closure has been executed.
      */
@@ -43,6 +67,17 @@ public struct Sometimes {
         return true
     }
     
+    /**
+     Removes all the stored keys.
+     */
+    public static func reset() { 
+        defaults.allKeys()
+            .filter { $0.hasPrefix(keyPrefix) }
+            .forEach { key in
+                defaults.removeBoolForKey(key)
+            }
+    }
+    
     // MARK: Private
     
     static let queue = dispatch_queue_create("com.alejandromp.sometimes", DISPATCH_QUEUE_SERIAL)
@@ -53,13 +88,11 @@ public struct Sometimes {
         dispatch_sync(queue) {
             // Check if it has been already executed
             
-            
-            
-            if NSUserDefaults.standardUserDefaults().boolForKey(key.valueKey) == false {
+            if defaults.boolForKey(key.valueKey) == false {
                 result = true
                 
                 // Update the value
-                NSUserDefaults.standardUserDefaults().setBool(true, forKey: key.valueKey)
+                defaults.setBool(true, forKey: key.valueKey)
             }
         }
         
--- SometimesTests/SometimesTests.swift
+++ SometimesTests/SometimesTests.swift
@@ -6,31 +6,119 @@
 //  Copyright © 2015 Alejandro Martinez. All rights reserved.
 //
 
-import XCTest
+
+import Quick
+import Nimble
+
 @testable import Sometimes
 
-class SometimesTests: XCTestCase {
+extension Dictionary: SometimesStore {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func boolForKey(defaultName: String) -> Bool {
+        return (self[defaultName as! Key] as? Bool) ?? false
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    mutating func setBool(value: Bool, forKey defaultName: String) {
+        self[defaultName as! Key] = (value as! Value)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    mutating func removeBoolForKey(defaultName: String) {
+        self[defaultName as! Key] = nil
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func allKeys() -> Set<String> {
+        return Set(self.map{ (key, value) in key as! String })
+    }
+}
+typealias FakeDefaults = Dictionary<String, Bool>
+
+class SometimesSpec: QuickSpec {
+    override func spec() {
+        
+        describe("Execute") {
+            
+            context("the first time") {
+                
+                beforeEach {
+                    Sometimes.defaults = FakeDefaults()
+                }
+                
+                it("should run the code") {
+                    let key = SometimesKey("test")
+                    
+                    var executed = false
+                    Sometimes.execute(key) {
+                        executed = true
+                    }
+                    
+                    expect(executed).toEventually(beTrue())
+                }
+            }
+            
+            context("the second time") {
+                
+                let key = SometimesKey("test")
+
+                beforeEach {
+                    Sometimes.defaults = FakeDefaults()
+                    Sometimes.execute(key) {}
+                }
+                
+                it("should not run the code") {
+                    
+                    var executed = false
+                    Sometimes.execute(key) {
+                        executed = true
+                    }
+                    
+                    expect(executed).toEventually(beFalse())
+                    
+                }
+                
+                
+            }
+        }
+        
+        describe("Clear") {
+            
+            var libKeys: Array<SometimesKey> = []
+            var otherKeys: Array<String> = []
+            
+            beforeEach {
+                // Prepare library and other app keys
+                libKeys = [
+                    SometimesKey("test1"),
+                    SometimesKey("test2")
+                ]
+                otherKeys = [
+                    "other1",
+                    "other2"
+                ]
+                
+                Sometimes.defaults = FakeDefaults()
+                
+                // Add all the keys
+                Sometimes.execute(libKeys[0]) {}
+                Sometimes.execute(libKeys[1]) {}
+                
+                Sometimes.defaults.setBool(true, forKey: otherKeys[0])
+                Sometimes.defaults.setBool(true, forKey: otherKeys[1])
+            }
+            
+            it("should remove all keys") {
+                Sometimes.reset()
+                
+                expect(Sometimes.defaults.boolForKey(libKeys[0].valueKey)).to(beFalse())
+                expect(Sometimes.defaults.boolForKey(libKeys[1].valueKey)).to(beFalse())
+            }
+            
+            it("should keep app keys") {
+                Sometimes.reset()
+                
+                expect(Sometimes.defaults.boolForKey(otherKeys[0])).to(beTrue())
+                expect(Sometimes.defaults.boolForKey(otherKeys[1])).to(beTrue())
+            }
+            
         }
     }
-    
 }
